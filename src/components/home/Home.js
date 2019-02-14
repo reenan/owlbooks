3269fb432de8cc1e92bac0df73102200b5de3c01
Douglas Collioni <douglas.collioni@sap.com>
2019-02-14 16:51:27 -0200

    initial commit for books table

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -6,6 +6,20 @@ const Home = () => (
     <header>
       <h2>My books</h2>
     </header>
+    <div className='list'>
+      <div className='item'>
+        <span>Harry Potter e a Pedra Filosofal</span>
+        <span>JK Rowling</span>
+        <span>Ficção</span>
+        <span>240</span>
+      </div>
+      <div className='item'>
+        <span>Fortaleza Digital</span>
+        <span>Dan Brown</span>
+        <span>Ficção</span>
+        <span>520</span>
+      </div>
+    </div>
   </div>
 )
 
