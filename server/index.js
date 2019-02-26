7f7e08a15773260a67b5fb6577ed771ae9ccd6b0
Douglas Collioni <douglas.collioni@sap.com>
2019-02-26 11:41:08 -0300

    refactoring server with new folder

--- server/index.js
+++ server/index.js
@@ -65,10 +65,10 @@ app.get('/api/books/:id', (req, res) => {
 })
 
 // if (process.env.NODE_ENV === 'production') {
-app.use(express.static(path.join(__dirname, 'client', 'build')))
+app.use(express.static(path.join(__dirname, '..', 'client', 'build')))
 
 app.get('*', (_req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+  res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'))
 })
 // }
 
