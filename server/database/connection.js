2fa6febfe25c90c9215c9b91ac4ed2f4c9fade2b
Douglas Collioni <douglas.collioni@sap.com>
2019-02-27 17:00:49 -0300

    change mongodb uri env var

--- server/database/connection.js
+++ server/database/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const connectionString = process.env.MONGODB_CONNECTION || 'mongodb://localhost:27017/owlbooks'
+const connectionString = process.env.MONGODB_URI || 'mongodb://localhost:27017/owlbooks'
 
 const connect = () => {
   return mongoose.connect(connectionString, { useNewUrlParser: true })
